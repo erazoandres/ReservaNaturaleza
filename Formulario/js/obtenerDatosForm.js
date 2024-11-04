b8705e614a5cbc17bb9335d804076feefaaaceec
Andrés Erazo <erazoandres14@gmail.com>
2024-11-04 14:38:58 -0500

    v3 Mejoras Agregadas de Sonia, Miguel , Nathaly

---
+++ Formulario/js/obtenerDatosForm.js
@@ -0,0 +1,32 @@
+function obtenerDatos(){
+    let nombre = document.getElementById("nombre").value;
+    let correo = document.getElementById("email").value;
+    let telefono = document.getElementById("telefono").value;
+    let ciudad = document.getElementById("ciudad").value;
+    let direccion = document.getElementById("direccion").value;
+    let comentarios = document.getElementById("comentarios").value.trim();
+    
+  alert("Tu nombre es: " + nombre  + "\n" +" tu email es: " + correo + "\n" +"Tu telefono es: " + telefono)
+
+// Para miguel solo ver en consola por que al igual el form formatea la informacion
+    console.log(nombre);   
+    console.log(correo);          
+    console.log(telefono);        
+    console.log(ciudad);      
+    console.log(direccion);        
+    console.log(comentarios);    
+
+    
+
+  } 
+
+
+
+
+  function crearElementoJson(){
+
+    const datosPalJson ={
+      nombreAtributo = 
+    }
+    
+  }
